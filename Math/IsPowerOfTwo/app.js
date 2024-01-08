8e5dcc6c2254a7782ea672a53ac4cd32cf7bc431
AdahMilly <oamildred@gmail.com>
2024-01-08 16:07:36 +0300

    is power of two implementation

--- Math/IsPowerOfTwo/app.js
+++ Math/IsPowerOfTwo/app.js
@@ -1,5 +1,28 @@
-function isPowerofTwo() {}
+function isPowerofTwo(number) {
+    if (number < 1) {
+        return false
+    }
+    let dividedNum = number;
+    while (dividedNum !== 1) {
+        if (dividedNum % 2 !== 0) {
+            return false
+        }
+        dividedNum = dividedNum / 2;
+    }
+    return true
+}
 
 console.log(isPowerofTwo(4));
 console.log(isPowerofTwo(5));
 console.log(isPowerofTwo(6));
+
+//best case: = o(1)
+//worst case: o(log n)
+
+//improved algorithm - bitwise implementation
+function isPowerOfTwoo(number) {
+    if (number < 1) {
+        return false
+    }
+    return (number & (number - 1)) === 0; //o(1)
+}
